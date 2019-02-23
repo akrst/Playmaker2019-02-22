744479dfbb9ddb66e9f4444f73c1f4d4cb7993f7
akrst1229 <akira.sato.1989@gmail.com>
2019-02-24 01:34:40 +0900

    EcosystemBrowser削除。ChangeMeshTopology、ChangeGrandChildrenMaterial、ObjectAnimationWrapper、TextureAnimationを追加。

---
+++ Assets/TextureAnimation/TextureArray.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureArray : MonoBehaviour {
+
+    //Animation用の連番のテクスチャ
+    public Texture[] textures;
+
+}
